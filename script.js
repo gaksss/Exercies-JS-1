006382cd471858eee6a8cdfd1a5ead9cc2a7821f
gaksss <clement.carlier2@gmail.com>
2024-11-13 13:56:32 +0100

    Exo5 fini

--- script.js
+++ script.js
@@ -13,3 +13,16 @@
 // let prenom = prompt("Entrez votre prénom : ");
 // let ville = prompt("Entrez votre ville : ");
 // alert("Nom : "+nom +"\n"+"Prénom : "+prenom+"\n"+"Ville : "+ville)
+
+// EXERCICE 5
+
+// let nombre1 = prompt("donne un nombre a virgule");
+// let nombre2 = prompt("donne un autre nombre a virgule");
+
+// let decimal1 = parseFloat(nombre1);
+// let decimal2 = parseFloat(nombre2);
+
+// decimal1 = Math.trunc(decimal1);
+// decimal2 = Math.trunc(decimal2);
+
+// alert("Le résultat est " + decimal1 * decimal2);
