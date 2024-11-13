cfc216c79b03d132804edb9950cd36eee17b793b
gaksss <clement.carlier2@gmail.com>
2024-11-13 11:57:48 +0100

    Exo4 fini

--- script.js
+++ script.js
@@ -4,6 +4,12 @@
 
 // EXERCICE 3
 
-// let prenom = prompt("Entrez votre prénom")
+// let prenom = prompt("Entrez votre prénom");
+// alert("Bonjour " + prenom);
 
-// alert("Bonjour " +prenom)
+// EXERCICE 4
+
+// let nom = prompt("Entrez votre nom : ") ;
+// let prenom = prompt("Entrez votre prénom : ");
+// let ville = prompt("Entrez votre ville : ");
+// alert("Nom : "+nom +"\n"+"Prénom : "+prenom+"\n"+"Ville : "+ville)
