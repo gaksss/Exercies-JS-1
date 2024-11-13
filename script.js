c30943b0cbd0b1de76e7df9725be716271a0b269
gaksss <clement.carlier2@gmail.com>
2024-11-13 11:45:50 +0100

    Exo3 fini

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let message = alert("Nom : Doe \nPrénom : John \nVille : New-York")
+// EXERCICE 1/2
+
+// let message = alert("Nom : Doe \nPrénom : John \nVille : New-York");
+
+// EXERCICE 3
+
+// let prenom = prompt("Entrez votre prénom")
+
+// alert("Bonjour " +prenom)
